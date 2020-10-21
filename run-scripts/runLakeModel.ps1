99a7cbec978281abb28517c9127b9649248eb373
carriemorrill <carrie.morrill@noaa.gov>
2020-10-21 11:39:27 -0600

    Add files via upload
    Powershell script

---
+++ run-scripts/runLakeModel.ps1
@@ -0,0 +1,15 @@
+#Powershell script
+#=================================================================
+
+gfortran -o parameter writeParameter.f90
+
+$j=1
+while($j -le 1){
+   Set-Content .\row.txt "$j"
+   ./parameter.exe
+   Get-Content lake.inc.save,fort.23 | Set-Content lake.inc
+   gfortran -o lake HB-windy-sed-varD2.f90
+   ./lake.exe
+   mv surface.dat surface-"$j".txt
+   $j++
+}   
