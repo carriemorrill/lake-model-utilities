6d27cc6855e56e94d6c22d237425daa00fb225ee
carriemorrill <carrie.morrill@noaa.gov>
2021-01-20 21:03:47 -0700

    Updated to handle subroutines in different files

--- run-scripts/runLakeModel.sh
+++ run-scripts/runLakeModel.sh
@@ -3,16 +3,23 @@
 
 #setenv WKDIR `pwd`
 
-f95 -o parameter writeParameter.f90
+cd Parameter-files
+gfortran -o parameter writeParameter.f90
+cd ..
 
 j=1
 while [ $j -le 1000 ]
 do
+   cd Parameter-files
    echo "$j" > row.txt
    ./parameter.exe
    cat lake.inc.save fort.23 > lake.inc
-   f95 -o lake env_sub.f90
+   mv lake.inc ..
+   cd ..
+   gfortran -c *.f90
+   gfortran -o lake *.o
    ./lake.exe
    mv surface.dat surface-"$j".txt
+   rm lake.inc
    ((j++)) 
 done
